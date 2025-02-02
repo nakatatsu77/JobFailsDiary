869116a2e1f55bcacd9268d3373b12bf2ee8a594
nakatatsu77 <nakatatsu1177@gmail.com>
2025-02-02 17:16:15 +0900

    静的OGPの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,29 @@ module ApplicationHelper
     else "bg-info"
     end
   end
+
+  def default_meta_tags
+    {
+      site: "バイトやらかしにっき",
+      title: "バイトやらかしにっき",
+      reverse: true,
+      charset: "utf-8",
+      description: "職場でのやらかしエピソードを投稿するアプリです",
+      canonical: request.original_url,
+      og: {
+        site_name: "バイトやらかしにっき",
+        title: "バイトやらかしにっき",
+        description: "職場でのやらかしエピソードを投稿するアプリです",
+        type: "website",
+        url: request.original_url,
+        image: image_url("post_placeholder.png"),
+        local: "ja-JP"
+      },
+      twitter: {
+        card: "summary_large_image",
+        site: "@https://x.com/WebTatsuya0707",
+        image: image_url("post_placeholder.png")
+      }
+    }
+  end
 end
